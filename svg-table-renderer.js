7264e4022ea217e7344159bad5e41325a877fdc2
Hendrik Strobelt <hendrik@strobelt.com>
2015-07-17 18:56:59 -0400

    stubs

---
+++ svg-table-renderer.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Hendrik Strobelt (hendrik.strobelt.com) on 7/17/15.
+ */
+(function(){
+
+    var svgTableRenderer ={}
+
+
+    if (typeof define === "function" && define.amd) define(svgTableRenderer); else if (typeof module === "object" && module.exports) module.exports = svgTableRenderer;
+    this.svgTableRenderer = svgTableRenderer;
+})();
+
+
